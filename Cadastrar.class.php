c20bf74af64069eaa75f0a1a312e9a25f6446cab
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2017-10-30 21:39:30 -0200

    Mudança na parte do cadastro usando md5
    Este parte usa a codificação de senha

--- Cadastrar.class.php
+++ Cadastrar.class.php
@@ -1,54 +1,32 @@
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Cadastro</title>
-      <link rel="stylesheet" href="css/style.css">
-    </head>
-  <body>
-  <form action="Cadastrar.class.php" method="post">
-    <div class="card">
-    <div class="field">
-      <span class="header">Cadastro</span>
-      <div class="form-group">
-      <input type="text" required="required"  maxlength="30" name="nome">
-        <label for="input" class="control-label" name="nome">username</label><i class="bar"></i>
-      </div>
-      <div class="form-group">
-        <input type="password" required="required" name="senha" maxlength="16">
-        <label for="input" class="control-label" name="senha">password</label><i class="bar"></i>
-      </div>
-      <div class="form-group">
-        <input type="text" required="required" maxlength="40" name="email">
-        <label for="input" class="control-label" name="email">email</label><i class="bar"></i>
-      </div>
-      <div class="button-container">
-        <input type="submit" class="button" value="Cadastrar" onclick="">
-      </div>
-  </html>
 <?php
-   require_once("Classes/ConexaoBD.class.php");
-   $objConBD = new ConexaoBD();
+  require_once('formCadastro.php');   
+  require_once('Classes/ConexaoBD.class.php');
 
-   //Pegar os dados digitados no form acima
+
    if(isset($_POST['nome']) && isset($_POST['senha']) && isset($_POST['email']))
    {
-     $nome  = $_POST['nome'];
-     $senha = $_POST['senha'];
-     $email = $_POST['email'];
 
-     $sql = "INSERT INTO usuario VALUES('$email','$senha','$nome')";
+    try{
+      $objCon = new ConexaoBD();
 
-     $result = sqlsrv_query($objConBD->con,$sql);
+      echo "Oi";
 
-     if($result == true)
-     {
-      // echo "Sucesooooooooo!";
-        header("Location:index.php");
-     }
-     else {
-        echo "<center>Erro no cadastro</center>";
-     }
+      $nome  = $_POST['nome'];
+      $senha = md5($_POST['senha']);
+      $email = $_POST['email'];
 
- }
+      $result = $objCon->insertUsuario($nome,$senha,$email);
+      
 
- ?>
+     if($result == true){     
+         header("Location:index.php"); 
+         }     
+     else{
+         echo "<center>Erro no cadastro</center>";
+        }
+      
+    }
+    catch(Exception $e)
+    {echo "Deu catch";}
+  }
+?>
